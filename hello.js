d8f050379c901ea7bde27774c840a109052d9f0c
Daniel Soares <danielsoares01@outlook.com>
2022-12-24 23:03:15 -0800

    committed two fundamental lessons

---
+++ hello.js
@@ -0,0 +1,21 @@
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+//console.log
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+//return
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
