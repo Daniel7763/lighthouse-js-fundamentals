754c4675238514bb0553e009d2beb776320180cc
Daniel Soares <danielsoares01@outlook.com>
2022-12-29 19:31:24 -0800

    ain

---
+++ howManyHundreds.js
@@ -0,0 +1,14 @@
+function howManyHundreds(x){
+  if (num = x / 100) {
+    return (Math.floor (num))
+  }
+  else {
+    return (Math.floor (num))
+  }
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
